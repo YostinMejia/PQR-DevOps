435cad77bbd7ef5cc251680d9bdca1f5cf514c29
Yostin <ysmaprogramming@gmail.com>
2026-03-01 14:51:19 -0500

    :tada: initial commit: setup pqr-app with spring web, jpa, and lombok

---
+++ src/main/java/com/devops/api/pqr/PqrApplication.java
@@ -0,0 +1,13 @@
+package com.devops.api.pqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrApplication.class, args);
+	}
+
+}
