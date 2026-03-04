cbe2678592b96cf8509ff85389d8beed8decbd3c
Yostin <ysmaprogramming@gmail.com>
2026-03-04 16:15:33 -0500

    :white_check_mark: feat(reviewer): add test for delete in te controller and service layer

--- src/test/java/com/devops/api/pqr/reviewer/ReviewerControllerTest.java
+++ src/test/java/com/devops/api/pqr/reviewer/ReviewerControllerTest.java
@@ -1,5 +1,6 @@
 package com.devops.api.pqr.reviewer;
 
+import com.devops.api.pqr.reviewer.dto.CreateReviewerDto;
 import com.devops.api.pqr.reviewer.entity.Reviewer;
 import com.devops.api.pqr.reviewer.mapper.ReviewerMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -7,13 +8,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -38,20 +43,60 @@ class ReviewerControllerTest {
     @DisplayName("POST /reviewer - Should create a new reviewer successfully")
     void testSaveShouldReturnCreatedWhenSavingReviewer() throws Exception {
         // GIVEN
-        Reviewer input = createReviewerRequest();
+        CreateReviewerDto dto = new CreateReviewerDto("pedro", "pedro@gmail.com");
         Reviewer expected = createReviewerResponse();
-
+        given(reviewerMapper.toReviewer(any(CreateReviewerDto.class))).willReturn(createReviewerRequest());
         given(reviewerService.saveReviewer(any(Reviewer.class))).willReturn(expected);
 
         // When
         // Then
         mockMvc.perform(post(BASE_URL)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(input)))
+                        .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(expected.getId()))
                 .andExpect(jsonPath("$.name").value(expected.getName()))
                 .andExpect(jsonPath("$.email").value(expected.getEmail()));
+    }
+
+    @Test
+    @DisplayName("POST /reviewer - Should return 400 when email is invalid")
+    void testSaveShouldReturnBadRequestWhenEmailIsInvalid() throws Exception {
+        // GIVEN
+        CreateReviewerDto invalidDto = new CreateReviewerDto("pedro", "email-invalido");
+
+        // WHEN
+        // THEN
+        mockMvc.perform(post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidDto)))
+                .andExpect(status().isBadRequest());
+
+    }
+    @Test
+    @DisplayName("DELETE /reviewer/{id} - Should return 200 when deleted successfully")
+    void testDeleteShouldReturnOkWhenReviewerExists() throws Exception {
+        // GIVEN
+        String id = "123";
+        // WHEN
+        // THEN
+        mockMvc.perform(delete(BASE_URL + "/" + id))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Reviewer deleted correctly"));
+    }
+
+    @Test
+    @DisplayName("DELETE /reviewer/{id} - Should return 400 when service throws exception")
+    void testDeleteShouldReturnBadRequestWhenIdNotFound() throws Exception {
+        // GIVEN
+        String id = "invalid-id";
+        willThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id not found"))
+                .given(reviewerService).delete(id);
+
+        // WHEN
+        // THEN
+        mockMvc.perform(delete(BASE_URL + "/" + id))
+                .andExpect(status().isBadRequest());
     }
 
     private Reviewer createReviewerRequest() {
